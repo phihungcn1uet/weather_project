c153915dd93271bef00592ee32fb3a9f4cf52ae8
Hung Nguyen <phihung18112005a@gmail.com>
2025-12-03 15:49:14 +0700

    Add more cities for handling

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import get_data
 import save_to_database
 import os
+import time 
 from dotenv import load_dotenv
 import alert
 import analyzer
 
 # get variable from .env file
 load_dotenv()
-CITY_NAME = os.getenv('CITY_NAME')
 API_KEY = os.getenv('API_KEY')
 DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
 
+# cities's list 
+CITIES = [
+    "Hanoi", 
+    "Ho Chi Minh City", 
+    "Da Nang", 
+    "Seoul", 
+    "Tokyo", 
+    "London", 
+    "New York"
+]
 
 def main():
     # check api key
@@ -23,23 +33,31 @@ def main():
         return
     
     # call data
-    cleaned_data = get_data.transform_data(CITY_NAME,API_KEY)
+    for CITY_NAME in CITIES:
+        try:
+            cleaned_data = get_data.transform_data(CITY_NAME,API_KEY)
 
-    # data handling
-    if cleaned_data:
-        # save data block
-        # save_to_database.save_log(cleaned_data, DB_CONNECTION_STRING)
-        print("Successful saving")
-        # alert block
-        warning_msg = alert.alert_condition(cleaned_data)
-        air_alert = analyzer.air_condition(cleaned_data['pm2_5_index'])
-        temp_alert = analyzer.feeling(cleaned_data['temperature'], cleaned_data['humidity'])
-        warning_msg += f'The air condition is {air_alert}\n'
-        warning_msg += f"Felling like {temp_alert[0]} and {temp_alert[1]}"
-        alert.send_message_to_devices(warning_msg)
+            # data handling
+            if cleaned_data:
+                # save data block
+                save_to_database.save_log(cleaned_data, DB_CONNECTION_STRING)
+                print("Successful saving")
+                
+                # alert block for ha noi weather
+                if(CITY_NAME == 'Hanoi'):
+                    warning_msg = alert.alert_condition(cleaned_data)
+                    air_alert = analyzer.air_condition(cleaned_data['pm2_5_index'])
+                    temp_alert = analyzer.feeling(cleaned_data['temperature'], cleaned_data['humidity'])
+                    warning_msg += f'The air condition is {air_alert}\n'
+                    warning_msg += f"Feeling like {temp_alert[0]} and {temp_alert[1]}"
+                    alert.send_message_to_devices(warning_msg)
 
-    else:
-        print("Failed to saving to the data")
+            else:
+                print("Failed to saving to the data")
+        except Exception as e:
+            print(f"   [ERROR] Lỗi tại {city}: {e}")
+            continue
+    print('swepping data successful')
 
 # start point of programming
 if __name__ == "__main__":
--- webapp.py
+++ webapp.py
@@ -4,8 +4,6 @@ from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 
-# web title 
-st.title("🌤️My weather data")
 
 # database connection
 load_dotenv()
@@ -30,23 +28,31 @@ def load_data():
 #display on screen
 st.write('Loading data from database')
 df = load_data()
+
 if df is not None and not df.empty:
-    lastest = df.iloc[0]
+    # slide bar for cities choosing
+    available_cities = df['city'].unique()
+    selected_city = st.sidebar.selectbox("📍 Cities:", available_cities)
+    city_df = df[df['city'] == selected_city]
+
+    latest = city_df.iloc[0]
+    st.title(f"🌤️ Weather in {selected_city}")
+
     col1,col2,col3 = st.columns(3)
-    col1.metric('City', lastest['city'])
-    col2.metric('Temperature',f'{lastest['temperature']}°C')
-    col3.metric('PM2.5', lastest['pm2_5_index'])
+    col1.metric('City', latest['city'])
+    col2.metric('Temperature',f'{round(latest['temperature'],2)}°C')
+    col3.metric('PM2.5', f'{latest['pm2_5_index']}µg/m³')
     # line chart for the fluctuation of temp per time
     st.subheader("Temperature fluctuation")
-    st.line_chart(df, x='time', y='temperature')
+    st.line_chart(city_df, x='time', y='temperature')
 
     #line chart for the fluctuation of pm2.5 per time
     st.subheader('pm 2.5 concentration per time')
-    st.line_chart(df,x='time', y='pm2_5_index')
+    st.line_chart(city_df,x='time', y='pm2_5_index')
 
     # raw data display
     st.subheader("Detailed data")
-    st.dataframe(df)
+    st.dataframe(city_df)
 else:
     st.warning("Database is empty")
 
